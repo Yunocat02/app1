144f45d2adad06fdea8843dc54ef5740237cf2a6
Chawanwit Kaenkong <s6303051623080@email.kmutnb.ac.th>
2023-09-08 15:54:26 +0700

    Sign in & Sign out

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { userContext } from './context'
-import Content from './context-content'
-import Headers from './context-header'
+import Header2 from './context-header2'
+import Content2 from './context-content2'
 export default function App() {
-return (
-<userContext.Provider value={'Tom Jerry'}>
-<Headers/>
-<Content/>
-</userContext.Provider>
-)
-}
+    let [user, setUser] = React.useState('')
+    return (
+    <userContext.Provider value={[user, setUser]}>
+    <Header2/>
+    <Content2/>
+    </userContext.Provider>
+    )
+    }
