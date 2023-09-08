8e0d9030242a8c8e03a2e269213025448ef114fb
Chawanwit Kaenkong <s6303051623080@email.kmutnb.ac.th>
2023-09-08 15:14:57 +0700

    make contect,header show together

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { userContext } from './context'
 import Content from './context-content'
+import Headers from './context-header'
 export default function App() {
 return (
 <userContext.Provider value={'Tom Jerry'}>
+<Headers/>
 <Content/>
 </userContext.Provider>
 )
